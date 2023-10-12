2691eeaca0c3b35da1713c2dd1e986ffed9995d2
Richard Olaniyan <34588531+rolani@users.noreply.github.com>
2023-10-12 21:14:39 +0000

    updated projects display

--- js/repo.js
+++ js/repo.js
@@ -17,11 +17,7 @@ fetch("repos.json")
         repo.url
       }"class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect bg-gray mdl-shadow--8dp"data-upgraded="MaterialButton,MaterialRipple"><i class="zmdi zmdi-github-alt"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></div><div class="mdl-card__actions mdl-card--border"><span class="blog-post-date inline-block">${
         repo.date.split("T")[0]
-      }</span><div class="mdl-layout-spacer"></div><div class="mdl-card__actions"><img class="mr-5"src="img/star.svg"/><span>${
-        repo.stars
-      }</span><img class="mr-5 ml-5"src="img/fork.svg"/><span>${
-        repo.forks
-      }</span></div></div></div></div><!--repo card ended-->`;
+      }</span><div class="mdl-layout-spacer"></div><div class="mdl-card__actions"><img class="mr-5"src="img/star.svg"/></div></div></div></div><!--repo card ended-->`;
     });
     document.getElementById("repo-card").innerHTML = output;
   });
